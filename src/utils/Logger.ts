48413df7f1509aa39d73c36753dedc2e5a9fa091
Fadi Atamny <fadiatamny@gmail.com>
2021-10-15 01:30:16 +0300

    changed order and fixed message inconsistensies in logger

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -16,16 +16,16 @@ export class Logger {
     public log(...log: any) {
         console.log(`(${this._getTime()}) [${this._name}]`, ...log)
     }
-    public error(...log: any) {
-        if (LogPriority[this.loggingLevel] < LogPriority[LogLevels.ERROR]) {
-            return
-        }
-        console.error(`(${this._getTime()}) [WARNING][${this._name}]`, ...log)
-    }
     public warn(...log: any) {
         if (LogPriority[this.loggingLevel] < LogPriority[LogLevels.WARN]) {
             return
         }
-        console.warn(`(${this._getTime()}) [ERROR][${this._name}]`, ...log)
+        console.warn(`(${this._getTime()}) [WARNING][${this._name}]`, ...log)
+    }
+    public error(...log: any) {
+        if (LogPriority[this.loggingLevel] < LogPriority[LogLevels.ERROR]) {
+            return
+        }
+        console.error(`(${this._getTime()}) [ERROR][${this._name}]`, ...log)
     }
 }
