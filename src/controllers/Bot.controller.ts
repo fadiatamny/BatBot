31e1893ae33f18847f2547141505ce4680f1df4b
Fadi Atamny <fadiatamny@gmail.com>
2021-10-15 03:38:46 +0300

    playing with presense message

--- src/controllers/Bot.controller.ts
+++ src/controllers/Bot.controller.ts
@@ -200,7 +200,7 @@ export default class BotController {
                 activities: [
                     {
                         name: presenceMessage,
-                        type: 'WATCHING'
+                        type: presenceMessage ? 'STREAMING' : 'WATCHING'
                     },
                     ...activities
                 ]
